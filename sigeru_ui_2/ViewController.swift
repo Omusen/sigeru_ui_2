ad74113d1a567e5e4272c1b5874fe480fc164982
yossiy <kokono1220@gmail.com>
2017-06-03 22:54:52 +0900

    Initial Commit

---
+++ sigeru_ui_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sigeru_ui_2
+//
+//  Created by yossiy_kkn on 2017/06/03.
+//  Copyright © 2017年 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
